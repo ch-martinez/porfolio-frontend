a61b528243fd0c5894a24f3636bd29912e309955
Christian <chmartinez.cba@gmail.com>
2022-08-08 12:10:16 -0300

    agregado login, agregado menu responsive

--- scripts/nav-button.js
+++ scripts/nav-button.js
@@ -1,7 +1,7 @@
 // activador de menu en mobile
-const navButton = document.querySelector(".nav-button");
-const navMenu = document.querySelector(".nav-menu");
+const navButton = document.querySelector(".nav__button");
+const navMenu = document.querySelector(".nav__menu");
 
 navButton.addEventListener("click", () =>{
-    navMenu.classList.toggle("nav-menu_visible");
+    navMenu.classList.toggle("nav__menu--visible");
 })
